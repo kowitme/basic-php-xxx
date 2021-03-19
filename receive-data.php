228214e270cd6b7f8ca546bc6262e534181122eb
Kowit Meboon <meboon_k@silpakorn.edu>
2021-03-19 17:15:43 +0700

    add select number for print time

--- receive-data.php
+++ receive-data.php
@@ -29,7 +29,13 @@
             echo "email = ".$_POST["email"];
             echo "<br>";
             echo "password = ".$_POST["pwd"];
+            echo "<br>";
+            
+            for ($x = 1; $x <= $_POST["time"]; $x++) {
+                echo "The number is: $x <br>";
+            }
         }
+
 
     ?>
     </div>
